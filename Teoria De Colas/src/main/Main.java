14d33d66db6de6cbdd8e0daaafb6ee6b60f01f8b
EpicJhon <epicjhon1994@gmail.com>
2014-03-04 11:32:13 -0500

    Add: Queueing theory

--- Teoria De Colas/src/main/Main.java
+++ Teoria De Colas/src/main/Main.java
@@ -5,7 +5,17 @@ import cola.Cola;
 public class Main {
 
 	public static void main(String[] args) {
-		Cola pila = new Cola();
-		pila.addItem("esto aun no es una pila");
+		Cola cola = new Cola();
+		cola.enqueue("soy el 1ro");
+		cola.enqueue("soy el 2do");
+		cola.enqueue("soy el 3ro");
+		cola.enqueue("soy el 4to");
+		cola.enqueue("soy el 5to");
+		cola.enqueue("soy el 6to");
+
+		String str;
+		while ((str = (String) cola.dequeue()) != null) {
+			System.out.println(str);
+		}
 	}
 }
