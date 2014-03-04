08618cc07e2e003bf9e71c71878b46f68b8e4351
JhonPolitecnico <jhonpolitecnico@gmail.com>
2014-03-03 20:10:47 -0500

    Pila!

--- Teoria De Pilas/src/main/Main.java
+++ Teoria De Pilas/src/main/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Pila pila = new Pila();
-		pila.addItem("esto aun no es una pila");
+		pila.push("Aqui");
+		pila.push(" ");
+		pila.push("Estamos");
+		pila.push(" ");
+		pila.push("Hola");
+
+		String str;
+		while ((str = (String) pila.pop()) != null) {
+			System.out.print(str);
+		}
 	}
 }
--- Teoria De Pilas/src/pila/Pila.java
+++ Teoria De Pilas/src/pila/Pila.java
@@ -2,56 +2,56 @@ package pila;
 
 public class Pila extends Nodo {
 
-	private int length;
-
 	public Pila() {
 		super(null);
 	}
 
-	public int getLength() {
-		return length;
+	// Push content
+	public void push(Object content) {
+
+		Nodo nodo = this;
+		if (nodo.hasRight())
+			nodo = getLastNode();
+
+		Nodo newNodo = new Nodo(content);
+		nodo.setRight(newNodo);
 	}
 
-	// Get node
-	private Nodo getNodo(int id) {
+	// Pop content
+	public Object pop() {
+		Nodo nodo = this;
 
-		// Null list
-		if (!this.hasRight())
+		if (!nodo.hasRight())
 			return null;
 
-		// No list element
-		if (id < 0 || id >= this.length)
-			return null;
+		// Get previous to the last node
+		while (nodo != null) {
 
-		Nodo nodo = this.getRight();
+			// Previous to the last node
+			if (nodo.hasRight()) {
+				if (!nodo.getRight().hasRight()) {
+					break;
+				}
 
-		for (int i = 0; i < id; i++) {
-			// Next node
+			}
+			
 			nodo = nodo.getRight();
 		}
 
-		return nodo;
-	}
+		// Next node
+		Nodo lastNode = nodo.getRight();
+		// Delete last node
+		nodo.setRight(null);
+		// Return last node
+		return lastNode.getContent();
 
-	public Object getItem(int id) {
-		Nodo nodo = this.getNodo(id);
-
-		if (nodo == null)
-			return null;
-
-		return nodo.getContent();
 	}
 
 	// Get last node
-	private Nodo getLastNodo() {
-
-		// Null list
-		if (!this.hasRight())
-			return null;
+	private Nodo getLastNode() {
 
 		Nodo nodo = this.getRight();
 
-		// Iterate nodes
 		while (nodo != null) {
 
 			// last node
@@ -63,136 +63,7 @@ public class Pila extends Nodo {
 		}
 
 		return nodo;
-	}
 
-	public void exchangeItems(int id, int targetId) {
-
-		// Null list
-		if (!this.hasRight())
-			return;
-
-		// No list element
-		if (id < 0 || id >= this.length || targetId < 0 || targetId >= this.length || id == targetId)
-			return;
-
-		// Link one (A)
-		int onePreviousId = id - 1;
-		Nodo onePreviousNode = null;
-
-		if (onePreviousId >= 0)
-			onePreviousNode = this.getNodo(id - 1);
-		else
-			onePreviousNode = this;
-
-		Nodo oneNode = this.getNodo(id); // Node A
-		Nodo oneNextNode = oneNode.getRight();
-
-		// Link two (B)
-		int twoPreviousId = targetId - 1;
-		Nodo twoPreviousNode = null;
-
-		if (twoPreviousId >= 0)
-			twoPreviousNode = this.getNodo(targetId - 1);
-		else
-			twoPreviousNode = this;
-
-		Nodo twoNode = this.getNodo(targetId); // Node B
-		Nodo twoNextNode = twoNode.getRight();
-
-		// exchange A to B
-
-		// B to A
-		onePreviousNode.setRight(twoNode);
-		twoNode.setRight(oneNextNode);
-
-		// A to B
-		twoPreviousNode.setRight(oneNode);
-		oneNode.setRight(twoNextNode);
-
-	}
-
-	public void removeItem(int id) {
-
-		Nodo nodo = this.getNodo(id);
-
-		if (nodo == null)
-			return;
-
-		// Link one
-		int previousId = id - 1;
-		Nodo previousNode = null;
-
-		if (previousId >= 0)
-			previousNode = this.getNodo(id - 1);
-		else
-			previousNode = this;
-
-		// Link two
-		Nodo nextNode = nodo.getRight();
-
-		// One to two
-		previousNode.setRight(nextNode);
-
-		// Decrement length
-		this.length--;
-	}
-
-	public void addItem(int id, Object content) {
-
-		// Null list
-		if (!this.hasRight())
-			return;
-
-		// No list element
-		if (id < 0 || id >= this.length)
-			return;
-
-		// Add node to last node
-		if (id == this.length) {
-			this.addItem(content);
-		} else {
-
-			// Link one
-			int previousId = id - 1;
-			Nodo previousNode = null;
-
-			if (previousId >= 0)
-				previousNode = this.getNodo(id - 1);
-			else
-				previousNode = this;
-
-			// Link two
-			Nodo nextNode = this.getNodo(id);
-
-			// New node
-			Nodo newNodo = new Nodo(content);
-
-			// link the new node to the node list
-			previousNode.setRight(newNodo);
-			newNodo.setRight(nextNode);
-
-			// Increment length
-			this.length++;
-		}
-
-	}
-
-	public void addItem(Object content) {
-		// Increment length
-		this.length++;
-
-		// Initial node
-		Nodo nodo = null;
-
-		// last node
-		if (this.getRight() != null)
-			nodo = this.getLastNodo();
-		else
-			nodo = this;
-
-		// Add node to last node
-		Nodo newNodo = new Nodo(content);
-		nodo.setRight(newNodo);
 	}
 
 }
