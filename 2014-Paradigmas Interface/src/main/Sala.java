a1cb215d7150a4b0259c57ec04fed151d2f60b88
JhonPolitecnico <jjeslava@COLPG25537.poligran.edu.co>
2014-03-22 13:27:13 -0500

    Update

--- 2014-Paradigmas Interface/src/main/Sala.java
+++ 2014-Paradigmas Interface/src/main/Sala.java
@@ -6,16 +6,17 @@ public class Sala {
 	private Hora horaInicio;
 	private Hora horaFin;
 	private ArrayList<Pelicula> peliculas;
+	private int sillas[][];
 
-	public Sala(Hora horaInicio, Hora horaFin) {
+	public Sala(Hora horaInicio, Hora horaFin, int noFilas, int NoColumnas) {
 		super();
 		peliculas = new ArrayList<Pelicula>();
 		this.horaInicio = horaInicio;
 		this.horaFin = horaFin;
+		this.sillas = new int[noFilas][NoColumnas];
 	}
 
 	public Hora getHoraInicio() {
-		System.out.println("Sala:" + horaInicio.getHora() + ":" + horaInicio.getMinuto() + ":" + horaInicio.getSegundo());
 		return horaInicio;
 	}
 
@@ -33,6 +34,37 @@ public class Sala {
 
 	public ArrayList<Pelicula> getPeliculas() {
 		return peliculas;
+	}
+
+	public int[][] getSillas() {
+		return sillas.clone();
+	}
+
+	public boolean AgregarPelicula(Pelicula peli) {
+
+		// Si la película esta en el horario de la this de cine
+		if ((peli.getHoraInicio().mayorQue(this.getHoraInicio()) || peli.getHoraInicio().igualQue(this.getHoraInicio()))
+				&& (peli.getHoraFin().menorQue(this.getHoraFin()) || peli.getHoraFin().igualQue(this.getHoraFin()))) {
+
+			if (this.getPeliculas().size() == 0) {
+				peli.setSillas(this.getSillas()); // Crear una copia de las sillas de la sala
+				this.getPeliculas().add(peli);
+				return true;
+			}
+
+			for (Pelicula sPeli : this.getPeliculas()) {
+
+				// Caso 1, Nueva peli luego de la peli antigua || Caso 2, Nueva
+				// peli antes de la peli antigua
+				if (!peli.getHoraInicio().mayorQue(sPeli.getHoraFin()) || !peli.getHoraFin().menorQue(sPeli.getHoraInicio()))
+					return false;
+
+			}
+
+			return true;
+		}
+
+		return false;
 	}
 
 }
