c16adfa3b006f43f4f7eee8fbaf5b257bc273098
EpicJhon <jjeslava@COLPG25635.poligran.edu.co>
2014-03-31 19:05:36 -0500

    Remove some comments

--- Polinomio de interpolacion Lagrange/src/main/Main.java
+++ Polinomio de interpolacion Lagrange/src/main/Main.java
@@ -9,21 +9,8 @@ public class Main {
 	 */
 
 	public static void main(String[] args) {
-
 		UI ui = new UI();
 		ui.start();
-
-		/*
-		 * L[] l = new L[3]; Point[] points = new Point[3]; P p;
-		 * 
-		 * points[0] = new Point(0, 7); points[1] = new Point(3, 7); points[2] = new Point(6, 6);
-		 * 
-		 * l[0] = new L(0, points); l[1] = new L(1, points); l[2] = new L(2, points);
-		 * 
-		 * p = new P(l, points);
-		 * 
-		 * System.out.println(p.toString());
-		 */
 	}
 
 }
