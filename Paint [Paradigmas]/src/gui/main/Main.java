2034644062fb7f84165b73a937085fb99de0e59f
JhonPolitecnico <jhonpolitecnico@gmail.com>
2014-05-03 12:55:50 -0500

    IDE

--- Paint [Paradigmas]/src/gui/main/Main.java
+++ Paint [Paradigmas]/src/gui/main/Main.java
@@ -38,7 +38,7 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 746, 457);
+		setBounds(100, 100, 560, 248);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -47,25 +47,52 @@ public class Main extends JFrame {
 		panel.setBackground(Color.ORANGE);
 		
 		JPanel panel_1 = new JPanel();
+		
+		JPanel panel_2 = new JPanel();
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 174, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 383, GroupLayout.PREFERRED_SIZE)
-					.addGap(116))
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 413, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+					.addGap(0))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(52)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 353, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
+						.addComponent(panel_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
+						.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE))
+					.addContainerGap())
 		);
+		
+		JButton button = new JButton("   ");
+		button.setBackground(Color.WHITE);
+		panel_2.add(button);
+		
+		JButton button_1 = new JButton("   ");
+		button_1.setBackground(Color.BLACK);
+		panel_2.add(button_1);
+		
+		JButton button_2 = new JButton("   ");
+		button_2.setBackground(Color.RED);
+		panel_2.add(button_2);
+		
+		JButton button_3 = new JButton("   ");
+		button_3.setBackground(Color.GREEN);
+		panel_2.add(button_3);
+		
+		JButton button_4 = new JButton("   ");
+		button_4.setBackground(Color.BLUE);
+		panel_2.add(button_4);
+		
+		JButton button_5 = new JButton("   ");
+		button_5.setBackground(Color.ORANGE);
+		panel_2.add(button_5);
 		
 		JButton btnLinea = new JButton("");
 		btnLinea.setIcon(new ImageIcon("C:\\Users\\jjeslava\\git\\Proyectos-2014\\Paint [Paradigmas]\\img\\line.png"));
