864f7b0016f05d1fb8915ec040542262b6acf4bb
JhonPolitecnico <jhonpolitecnico@gmail.com>
2014-05-15 17:57:14 -0500

    fixed serial ID

--- MIDIPlayer/src/library/Library.java
+++ MIDIPlayer/src/library/Library.java
@@ -5,7 +5,13 @@ import java.io.Serializable;
 import midi.MIDIPLayList;
 import midi.SongList;
 
+/**
+ * Skeleton file library
+ * 
+ */
 public class Library implements Serializable {
+
+	private static final long serialVersionUID = -3340855410454139702L;
 
 	private SongList songList;
 	private MIDIPLayList playlists;
