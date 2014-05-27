df74dbf09e3972907b770024e2551a2d187d5916
JhonPolitecnico <jhonpolitecnico@gmail.com>
2014-05-26 20:51:36 -0500

    added

--- Binary/src/binary/Binary.java
+++ Binary/src/binary/Binary.java
@@ -2,18 +2,33 @@ package binary;
 
 public class Binary {
 
-	public static int DecinalToBinary(int num) {
+	public static int decinalToBinary(int num) {
 
 		String binary = "";
 
 		while (true) {
 			binary += String.valueOf(num % 2);
-			
-			System.out.println(num);
-			
+
 			if (num < 2)
 				break;
-			
+
+			num /= 2;
+
+		}
+
+		return Integer.parseInt(new StringBuilder(binary).reverse().toString());
+	}
+
+	public static int binarySum(int num, int num2) {
+
+		String binary = "";
+
+		while (true) {
+			binary += String.valueOf(num % 2);
+
+			if (num < 2)
+				break;
+
 			num /= 2;
 
 		}
--- Binary/src/main/Main.java
+++ Binary/src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println(Binary.DecinalToBinary(7));
+		System.out.println(Binary.decinalToBinary(346));
 	}
 
 }
